cea456e4f7a00df47f97cdf19c6e2784e9abaae9
Gabriel Blank Stift Mousquer <gabrielblanksm@gmail.com>
2024-12-15 03:15:13 -0300

    formatting

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -1,5 +1,5 @@
 (ns advent_of_code.core
-    (:require [advent_of_code.day1 :as day1]))
+  (:require [advent_of_code.day1 :as day1]))
 
 (defn -main [& args]
-    (day1/solution (first args)))
+  (day1/solution (first args)))
--- src/advent_of_code/day1.clj
+++ src/advent_of_code/day1.clj
@@ -1,29 +1,29 @@
 (ns advent_of_code.day1
-    (:require
-        [clojure.java.io :as io]
-        [clojure.string :as str]))
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as str]))
 
 (defn solution [filepath]
-    (println "Day 1")
-    (with-open [reader (io/reader filepath)]
-        (->> (line-seq reader)
-            (filter (complement empty?))
-            (map #(str/split % #"\s+"))
-            (apply map vector)
-            (map (comp (partial map Integer/parseInt) sort))
-            (apply map -)
-            (map Math/abs)
-            (reduce +)
-            (println "Part 1:")))
-    (with-open [reader (io/reader filepath)]
-        (->> (line-seq reader)
-            (filter (complement empty?))
-            (map #(str/split % #"\s+"))
-            (apply map vector)
-            (map (partial map Integer/parseInt))
-            ((fn [[list1 list2]]
-                (let [freq-table (frequencies list2)
-                    freq (map #(get freq-table % 0) list1)]
-                    (map * freq list1))))
-            (reduce +)
-            (println "Part 2:"))))
+  (println "Day 1")
+  (with-open [reader (io/reader filepath)]
+    (->> (line-seq reader)
+         (filter (complement empty?))
+         (map #(str/split % #"\s+"))
+         (apply map vector)
+         (map (comp (partial map Integer/parseInt) sort))
+         (apply map -)
+         (map Math/abs)
+         (reduce +)
+         (println "Part 1:")))
+  (with-open [reader (io/reader filepath)]
+    (->> (line-seq reader)
+         (filter (complement empty?))
+         (map #(str/split % #"\s+"))
+         (apply map vector)
+         (map (partial map Integer/parseInt))
+         ((fn [[list1 list2]]
+            (let [freq-table (frequencies list2)
+                  freq (map #(get freq-table % 0) list1)]
+              (map * freq list1))))
+         (reduce +)
+         (println "Part 2:"))))
